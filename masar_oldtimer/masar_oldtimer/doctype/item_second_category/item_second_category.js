7802c74e908cac4338d6bc9d6bff9bfa984728c7
karamakcsc <karamakcsc@hotmail.com>
2022-01-20 19:54:06 +0000

    first_upload

---
+++ masar_oldtimer/masar_oldtimer/doctype/item_second_category/item_second_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, KCSC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Second Category', {
+	// refresh: function(frm) {
+
+	// }
+});
