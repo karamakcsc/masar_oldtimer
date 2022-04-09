bd4e5c0bba69148e891fdd1c65de0b7fef37bcd0
karamakcsc <karamakcsc@hotmail.com>
2022-04-09 20:37:43 +0000

    search by wh

--- masar_oldtimer/masar_oldtimer/doctype/item_location/item_location.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, KCSC and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ItemLocation(Document):
-	pass
