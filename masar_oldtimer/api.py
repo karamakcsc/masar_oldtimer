ac5089b533ea4d36aaaece325e0d3f993253725a
karamakcsc <karamakcsc@hotmail.com>
2022-07-17 07:43:13 +0000

    API

--- masar_oldtimer/api.py
+++ masar_oldtimer/api.py
@@ -7,4 +7,12 @@ def get_item_details(item=None):
 							inner JOIN `tabItem Price` tip
 							ON (tip.item_code = ti.item_code AND tip.brand =ti.brand)
 							WHERE ti.disabled= 0 AND ti.brand = 'Baja Designs' AND ti.publish_on_bajadesigns = 1 AND tip.price_list = 'BajaDesigns MSRP -USD'
-							ORDER BY ti.creation DESC LIMIT 12;""", as_dict=True)
+							ORDER BY ti.creation DESC;""", as_dict=True)
+
+
+
+
+# @frappe.whitelist()
+# def get_second_cat(cat_nd=None):
+# 	return frappe.db.sql(""" SELECT name, creation, first_category, second_category
+# 						   FROM `tabItem Second Category`  WHERE first_category ='Cars' ORDER BY creation DESC ;""", as_dict=True)
