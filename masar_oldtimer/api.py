ebd1442ebb163452db66d21a9990f8a61f1875c3
karamakcsc <karamakcsc@hotmail.com>
2022-07-18 10:05:05 +0000

    API4

--- masar_oldtimer/api.py
+++ masar_oldtimer/api.py
@@ -1,19 +1,22 @@
 import frappe
 import requests , json
 
-# @frappe.whitelist()
-# def get_item_details(item=None):
-# 	return frappe.db.sql(""" SELECT ti.name, ti.item_name, ti.item_code, ti.description, ti.image, ti.disabled, ti.publish_on_bajadesigns,
-# 								tip.price_list, price_list_rate, tisc.second_category, titc.third_category
-# 							FROM `tabItem` ti
-# 							inner JOIN `tabItem Price` tip
-# 							ON (tip.item_code = ti.item_code AND tip.brand =ti.brand)
-# 							LEFT JOIN `tabItem Second Category Value` tisc
-# 							ON ti.item_code = tisc.second_category
-# 							LEFT JOIN `tabItem Third Category Value` titc
-# 							ON ti.item_code = titc.parent
-# 							WHERE ti.disabled= 0 AND ti.brand = 'Baja Designs' AND ti.publish_on_bajadesigns = 1 AND tip.price_list = 'BajaDesigns MSRP -USD'
-# 							ORDER BY ti.creation DESC;""", as_dict=True)
+@frappe.whitelist()
+def get_item_details(item=None):
+	return frappe.db.sql(""" SELECT * ,tf.file_url
+							FROM `tabItem` ti
+							Inner Join `tabFile` tf
+							ON ti.item_code = tf.attached_to_name
+							WHERE ti.disabled= 0 AND ti.brand = 'Baja Designs' AND ti.publish_on_bajadesigns = 1
+							ORDER BY ti.creation DESC;""", as_dict=True)
+
+
+@frappe.whitelist()
+def get_variants_details(variant=None):
+	return frappe.db.sql(""" SELECT *
+							FROM `tabItem`
+							WHERE disabled= 0 AND brand = 'Baja Designs' AND publish_on_bajadesigns = 1 AND variant_of != ""
+							ORDER BY creation DESC;""", as_dict=True)
 
 
 
@@ -37,11 +40,3 @@ import requests , json
 # 		items = frappe.db.sql("""select c.item_code, i.third_category from `tabItem` as c left join `tabItem Third Category Value` as i on c.item_code =i.parent
 # 										 ORDER BY i.creation DESC ;""", as_dict=True)
 # 		return items
-
-
-@frappe.whitelist()
-def get_item_details(item=None):
-	return frappe.db.sql(""" SELECT *
-							FROM `tabItem`
-							WHERE disabled= 0 AND brand = 'Baja Designs' AND publish_on_bajadesigns = 1
-							ORDER BY creation DESC;""", as_dict=True)
